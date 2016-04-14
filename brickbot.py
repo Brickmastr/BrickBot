d419d7946e1bfd3141ca8afbb3c5687fcc522a7f
Nathan Tenney <nathant@ramcompany.com>
2016-04-14 11:22:20 -0600

    Added a debug function

--- brickbot.py
+++ brickbot.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 import config
 from discord.ext import commands
+from cogs.utils import checks
 
 
 description = 'Brickmastr\'s personal Discord Bot.'
@@ -32,12 +33,33 @@ def on_ready():
             print('Failed to load extension {}\n{} {}'.format(extension, type(e).__name__, e))
 
 
+@bot.command(pass_context=True, hidden=True)
+@checks.is_owner()
+@asyncio.coroutine
+def debug(ctx, *, code: str):
+    """Evaluates code."""
+    code = code.strip('` ')
+    python = '```py\n{}\n```'
+    result = None
+
+    try:
+        result = eval(code)
+    except Exception as e:
+        yield from bot.say(python.format(type(e).__name__ + ': ' + str(e)))
+        return
+
+    if asyncio.iscoroutine(result):
+        result = yield from result
+
+    yield from bot.say(python.format(result))
+
+
 while True:
     try:
         bot.run(config.TOKEN)
     except KeyboardInterrupt:
         print('Interrupted. Signing out.')
         break
-    except TimeoutError:
+    except (TimeoutError, RuntimeError):
         print('Connection Timed Out. Reconnecting in 15 seconds...')
         time.sleep(15)
